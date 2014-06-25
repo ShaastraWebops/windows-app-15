f38906c29d4ad597811e8e4f5b8786dcd9a7ea93
nishanthkarthik <nishanthkarthik@live.com>
2014-06-25 20:59:24 +0530

    Initial Commit
    Page transitions, flip animation for lecture page

---
+++ lectures_page/lectures_page/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using lectures_page.Resources;
+
+namespace lectures_page
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
