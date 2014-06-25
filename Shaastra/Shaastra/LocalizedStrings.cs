449ebb8f102edb20731d2d1e80fd258a67d5bf5f
nishanthkarthik <nishanthkarthik@live.com>
2014-06-25 21:34:01 +0530

    Main app
    First Commit, has lectures page, with transitions and flip animation

---
+++ Shaastra/Shaastra/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Shaastra.Resources;
+
+namespace Shaastra
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
